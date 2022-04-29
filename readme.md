1a74f3f3cc4d8018274736f62ecfa1ea30138fdb
Marcel Witoschek <marcel@marciland.de>
2022-04-30 01:32:06 +0200

    added variable run script

--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
 Idle Champions:\
 Only works if game client is set to fullscreen and 100% ui size on 1920/1080 \
-Use LANGUAGE:de/en to choose task execution based on game language\
-To run use "python.exe run.py"\
+To run use "python.exe run.py language Suite"\
--- run.py
+++ run.py
@@ -1,2 +1,11 @@
 import subprocess
-subprocess.run("powershell.exe robot --variable LANGUAGE:de --outputdir ${CURDIR}${/}result Task/IdleChamps.robot")
+import sys
+
+def run(language, suite):
+    command = "powershell.exe robot --variable LANGUAGE:"+language+" --outputdir ${CURDIR}${/}result Task/"+suite+".robot"
+    subprocess.run(command)
+
+if __name__ == "__main__":
+    language = str(sys.argv[1])
+    suite = str(sys.argv[2])
+    run(language, suite)
